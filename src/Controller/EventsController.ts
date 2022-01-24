d76c78240cc0c8bf9afd57e4c91581486593aea9
Karol Sójko <karolsojko@protonmail.com>
2022-01-24 17:16:45 +0100

    fix: add wait-for script and logs

--- src/Controller/EventsController.ts
+++ src/Controller/EventsController.ts
@@ -4,18 +4,22 @@ import { TimerInterface } from '@standardnotes/time'
 import { inject } from 'inversify'
 import { BaseHttpController, controller, httpPost, results } from 'inversify-express-utils'
 import TYPES from '../Bootstrap/Types'
+import { Logger } from 'winston'
 
 @controller('/events')
 export class EventsController extends BaseHttpController {
   constructor(
     @inject(TYPES.DomainEventPublisher) private domainEventPublisher: DomainEventPublisherInterface,
     @inject(TYPES.Timer) private timer: TimerInterface,
+    @inject(TYPES.Logger) private logger: Logger,
   ) {
     super()
   }
 
   @httpPost('/')
   async publishEvent(request: Request): Promise<results.JsonResult> {
+    this.logger.info(`Received event ${request.body.eventType} with payload: ${JSON.stringify(request.body.eventPayload)}`)
+
     await this.domainEventPublisher.publish({
       type: request.body.eventType as string,
       createdAt: this.timer.getUTCDate(),
